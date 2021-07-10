76e0fc77be4aecc95928cfe902ca16c36d42805e
Carlolis <cilieff@gmail.com>
2021-07-10 11:28:11 +0200

    Add contact Info Properties to Owner. closes #28

--- src/main/java/fr/ilieff/sfgpetclinic/model/Owner.java
+++ src/main/java/fr/ilieff/sfgpetclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package fr.ilieff.sfgpetclinic.model;
 
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Owner extends Person {
+
+  private String city;
+  private String adresse;
+  private String telephone;
+  private Set<Pet> pets;
 
 }
