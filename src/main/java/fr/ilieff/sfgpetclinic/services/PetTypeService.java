7e5dc185596b4cf0cba5c88484d4d33c308fac7c
Carlolis <cilieff@gmail.com>
2021-07-10 17:59:32 +0200

    added PetTypeService. closes #29

---
+++ src/main/java/fr/ilieff/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package fr.ilieff.sfgpetclinic.services;
+
+import fr.ilieff.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ src/main/java/fr/ilieff/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,42 @@
+package fr.ilieff.sfgpetclinic.services.map;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import fr.ilieff.sfgpetclinic.model.PetType;
+import fr.ilieff.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+  @Override
+  public PetType findById(Long id) {
+
+    return super.findById(id);
+  }
+
+
+  @Override
+  public PetType save(PetType object) {
+
+    return super.save(object);
+  }
+
+  @Override
+  public void delete(PetType object) {
+    super.delete(object);
+
+  }
+
+  @Override
+  public void deleteByID(Long id) {
+    super.deleteByID(id);
+  }
+
+  @Override
+  public Set<PetType> findAll() {
+    return super.findAll();
+  }
+
+
+
+}
