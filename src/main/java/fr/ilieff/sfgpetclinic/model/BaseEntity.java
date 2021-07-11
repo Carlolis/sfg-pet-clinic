01ac00bb9cec5e5dd0b65b15c9e9be18794a235d
Carlolis <cilieff@gmail.com>
2021-07-11 11:47:35 +0200

    Implement BaseEntity JPA. closes #35

--- src/main/java/fr/ilieff/sfgpetclinic/model/BaseEntity.java
+++ src/main/java/fr/ilieff/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package fr.ilieff.sfgpetclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class BaseEntity implements Serializable {
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 }
