b8aec7a15034c79d1206c93d8765e787ac79c8e2
Carlolis <cilieff@gmail.com>
2021-07-11 17:27:48 +0200

    Create Visit Entity. closes #38

--- src/main/java/fr/ilieff/sfgpetclinic/model/Pet.java
+++ src/main/java/fr/ilieff/sfgpetclinic/model/Pet.java
@@ -1,9 +1,13 @@
 package fr.ilieff.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,4 +29,7 @@ public class Pet extends BaseEntity {
   @ManyToOne
   @JoinColumn(name = "owner_id")
   private Owner owner;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+  private Set<Visit> visits = new HashSet<>();
 }
--- src/main/java/fr/ilieff/sfgpetclinic/model/Visit.java
+++ src/main/java/fr/ilieff/sfgpetclinic/model/Visit.java
@@ -1,14 +1,22 @@
 package fr.ilieff.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@Entity
 public class Visit extends BaseEntity {
+
   private LocalDate date;
   private String description;
+
+  @ManyToOne
+  @JoinColumn(name = "pet_id")
   private Pet pet;
 }
