330eb252e88985a0812cb937f9688404c404d563
Carlolis <cilieff@gmail.com>
2021-07-14 11:53:51 +0200

    Create Visit Map Service closes #46

---
+++ src/main/java/fr/ilieff/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package fr.ilieff.sfgpetclinic.services;
+
+import fr.ilieff.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ src/main/java/fr/ilieff/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,49 @@
+package fr.ilieff.sfgpetclinic.services.map;
+
+import java.util.Set;
+import fr.ilieff.sfgpetclinic.model.Visit;
+import fr.ilieff.sfgpetclinic.services.VisitService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+
+  private final VisitService visitService;
+
+  @Override
+  public Set<Visit> findAll() {
+
+    return super.findAll();
+  }
+
+  @Override
+  public Visit findById(Long id) {
+
+    return super.findById(id);
+  }
+
+  @Override
+  public Visit save(Visit visit) {
+    if (visit.getPet() == null || visit.getPet().getOwner() == null
+        || visit.getPet().getId() == null) {
+      throw new RuntimeException("Invalid vist");
+    }
+
+    return super.save(visit);
+  }
+
+  @Override
+  public void delete(Visit object) {
+    super.delete(object);
+
+  }
+
+  @Override
+  public void deleteByID(Long id) {
+    super.deleteByID(id);
+
+  }
+
+}
